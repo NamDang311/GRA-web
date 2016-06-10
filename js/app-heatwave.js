4ade6d3699e46bd80f52baa824cd143b6e399db8
NamDang311 <namdang.h311@gmail.com>
2016-06-11 03:01:11 +1000

    asd

--- js/app-heatwave.js
+++ js/app-heatwave.js
@@ -4,8 +4,8 @@ $(".main").onepage_scroll({
     pagination: true
     , loop: false
 });
-plyr.setup(); 
-//SEC-11
+plyr.setup();
+//SEC-1
 $(".msg-wrap h3").hover(function () {
     TweenMax.fromTo(".msg-wrap .line", 0.3, {
         scaleX: 0
@@ -25,10 +25,74 @@ $(".msg-wrap h3").hover(function () {
 }, function () {
 
 });
-//END-SEC-11
 
 var defaults = {
-  value: 2850276, inc: 1, pace: 2000, auto: true
+    value: 2850276
+    , inc: 1
+    , pace: 2000
+    , auto: true
 };
 
 var c1 = new flipCounter('c1', defaults);
+
+//SEC-2
+var tlwaterdrop = new TimelineMax({
+    repeat: -1
+});
+var tlwaterdrop2 = new TimelineMax({
+    repeat: -1,
+});
+var checkHandler=true;
+function checktoggler(){
+    if(checkHandler==false){
+        tlwaterdrop.pause();
+        tlwaterdrop2.pause();
+    }
+};
+Snap.load("img/savetheearth/faucet.svg", function (data) {
+    Snap("#water-faucet").append(data);
+    tlwaterdrop.fromTo(".waterdrop_0", 4, {
+        y: "-=30px"
+    }, {
+        y: "+=30px"
+        , ease: Power1.easeIn
+    }, 0).to(".waterdrop_0", 0.5, {
+        y: "+=800px"
+        , ease: Power1.easeIn,onComplete:function(){checktoggler()}
+    });
+    tlwaterdrop2.fromTo(".waterdrop_1", 3, {
+        y: "-=30px"
+    }, {
+        y: "+=30px"
+        , ease: Power1.easeIn
+    }, 1).to(".waterdrop_1", 0.5, {
+        y: "+=800px"
+        , ease: Power1.easeIn
+    }).
+    fromTo(".waterdrop_1", 1, {
+        y: "-=30px"
+    }, {
+        y: "+=800px"
+        , ease: Power1.easeIn,onComplete:function(){checktoggler()}
+    }, "+=0.5");
+    TweenLite.set(".waterHandler", {
+        transformOrigin: "6.483px 99px"
+    })
+    Draggable.create(".waterHandler", {
+        type: "rotation"
+        , trigger: ".a9a13da9-9345-46d1-a320-c9b71331183a"
+        , throwProps: false
+        , dragResistance: 0.55
+        , bounds: {
+            minRotation: 0
+            , maxRotation: -34.31
+        },
+        throwResistance:10000
+        , onDragEnd: function () {
+            
+            if(Draggable.get(".waterHandler").target._gsTransform.rotation == -34.31){checkHandler=false,console.log(checkHandler)};
+        }
+    });
+
+
+});
